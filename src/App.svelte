27249ae9249e3eec036cce7394eddfa4138eaa61
Carson Chen <c4r50n@Carsons-Mac-mini.local>
2025-02-02 09:45:57 +1100

    feat: add dummy data for meetups

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,27 @@
 <script>
   import Header from './UI/Header.svelte';
+  import MeetupItem from './Meetups/MeetupItem.svelte';
+
+  const MEETUPS = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description: 'Descriptions for coding bootcamp',
+      imageUrl: 'coding image',
+      address: 'Dummy Address Road, 4242',
+      contactEmail: 'code@test.com',
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: "Let's go for some swimming",
+      description: 'Descriptions for swim together',
+      imageUrl: 'swimming image',
+      address: 'Dummy Address Road, 1234',
+      contactEmail: 'swim@test.com',
+    },
+  ];
 </script>
 
 <Header />
