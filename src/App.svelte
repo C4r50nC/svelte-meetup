9d98f3f539d7e7806db3b9f695d40236b029c7d5
C4r50nC <xichen059@gmail.com>
2025-02-04 08:16:29 +1100

    feat: add form for inserting new meetups

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,14 @@
   import Header from './UI/Header.svelte';
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
 
-  const MEETUPS = [
+  let title = '';
+  let subtitle = '';
+  let address = '';
+  let email = '';
+  let description = '';
+  let imageUrl = '';
+
+  let meetups = [
     {
       id: 'm1',
       title: 'Coding Bootcamp',
@@ -22,12 +29,59 @@
       contactEmail: 'swim@test.com',
     },
   ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      address: address,
+      contactEmail: email,
+    };
+
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
 <Header />
 
 <main>
-  <MeetupGrid meetups={MEETUPS} />
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+    <div class="form-control">
+      <label for="address">Address</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="image-url">Image URL</label>
+      <input type="text" id="image-url" bind:value={imageUrl} />
+    </div>
+    <div class="form-control">
+      <label for="email">E-Mail</label>
+      <input type="email" id="email" bind:value={email} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea
+        rows="3"
+        name="description"
+        id="description"
+        bind:value={description}
+      ></textarea>
+    </div>
+    <button type="submit">Save</button>
+  </form>
+
+  <MeetupGrid {meetups} />
 </main>
 
 <style>
