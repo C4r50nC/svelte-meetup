4074190b11f8bb691823a7db280492c9b436533b
C4r50nC <xichen059@gmail.com>
2025-02-07 08:03:59 +1100

    feat: add real images for sample data

--- src/App.svelte
+++ src/App.svelte
@@ -17,7 +17,8 @@
       title: 'Coding Bootcamp',
       subtitle: 'Learn to code in 2 hours',
       description: 'Descriptions for coding bootcamp',
-      imageUrl: 'coding image',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG',
       address: 'Dummy Address Road, 4242',
       contactEmail: 'code@test.com',
     },
@@ -26,7 +27,8 @@
       title: 'Swim Together',
       subtitle: "Let's go for some swimming",
       description: 'Descriptions for swim together',
-      imageUrl: 'swimming image',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg',
       address: 'Dummy Address Road, 1234',
       contactEmail: 'swim@test.com',
     },
