d61cf4be0528557418d9e4608d9ca2e850e94700
C4r50nC <xichen059@gmail.com>
2025-02-15 14:29:14 +1100

    refactor: add default value for parameters to suppress warning when they are unused

--- src/App.svelte
+++ src/App.svelte
@@ -56,31 +56,31 @@
     <TextInput
       id="title"
       label="Title"
-      type="text"
       value={title}
       on:input={(event) => (title = event.target.value)}
     />
+
     <TextInput
       id="subtitle"
       label="Subtitle"
-      type="text"
       value={subtitle}
       on:input={(event) => (subtitle = event.target.value)}
     />
+
     <TextInput
       id="address"
       label="Address"
-      type="text"
       value={address}
       on:input={(event) => (address = event.target.value)}
     />
+
     <TextInput
       id="image-url"
       label="Image URL"
-      type="text"
       value={imageUrl}
       on:input={(event) => (imageUrl = event.target.value)}
     />
+
     <TextInput
       id="email"
       label="E-Mail"
@@ -88,6 +88,7 @@
       value={email}
       on:input={(event) => (email = event.target.value)}
     />
+
     <TextInput
       id="description"
       label="description"
@@ -96,6 +97,7 @@
       controlType="textarea"
       on:input={(event) => (description = event.target.value)}
     />
+
     <Button type="submit" caption="Save" />
   </form>
 
