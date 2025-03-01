3a1e1f1a66ba4a97d74ec8d6ed8d0e13e686cea9
C4r50nC <xichen059@gmail.com>
2025-03-01 18:57:57 +1100

    refactor: remove unnecessary variable declarations

--- src/Meetups/EditMeetup.svelte
+++ src/Meetups/EditMeetup.svelte
@@ -8,17 +8,11 @@
   import { isEmpty, isValidEmail } from '../helpers/validation';
 
   let title = '';
-  let titleValid = false;
   let subtitle = '';
-  let subtitleValid = false;
   let address = '';
-  let addressValid = false;
   let email = '';
-  let emailValid = false;
   let description = '';
-  let descriptionValid = false;
   let imageUrl = '';
-  let imageUrlValid = false;
   let formIsValid = false;
 
   const dispatch = createEventDispatcher();
