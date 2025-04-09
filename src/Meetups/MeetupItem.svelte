ebc9550d4a3188eecc16e6b21f4e6a1fe402ae83
C4r50nC <xichen059@gmail.com>
2025-04-10 07:50:32 +1000

    feat: connect favorite meetup to Firebase

--- src/Meetups/MeetupItem.svelte
+++ src/Meetups/MeetupItem.svelte
@@ -17,7 +17,19 @@
   const dispatch = createEventDispatcher();
 
   function toggleFavorite() {
-    meetups.toggleFavorite(id);
+    fetch(
+      `https://svelte-http-ad86f-default-rtdb.asia-southeast1.firebasedatabase.app/meetups/${id}.json`,
+      {
+        method: 'PATCH',
+        body: JSON.stringify({ isFavorite: !isFavorite }),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) throw new Error('An error occurred, please try again!');
+        meetups.toggleFavorite(id);
+      })
+      .catch((err) => console.log(err));
   }
 </script>
 
